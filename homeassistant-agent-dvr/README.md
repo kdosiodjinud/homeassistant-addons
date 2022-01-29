667f197a49dda9fabf8c5054df41a68037961fdb
Jan Kuthan <jan.kuthan@lmc.eu>
2022-01-29 18:37:38 +0100

    Test commit

--- homeassistant-agent-dvr/README.md
+++ homeassistant-agent-dvr/README.md
@@ -2,3 +2,7 @@
 ## Setup
 // todo
 
+docker buildx build --platform linux/arm/v7 -t kdosiodjinud/homeassistant-agent-dvr:1.0.0 . --no-cache
+
+COPY install.sh ./install.sh
+RUN chmod a+x ./install.sh && ./install.sh
--- homeassistant-agent-dvr/install.sh
+++ homeassistant-agent-dvr/install.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-echo "installing build tools" && \
-	apt-get update && \
-	apt-get install -y unzip python3 curl make g++ build-essential libvlc-dev vlc libx11-dev ffmpeg build-essential cmake git unzip pkg-config gfortran libjpeg-dev libtiff-dev libpng-dev libavcodec-dev libavformat-dev libswscale-dev libgtk2.0-dev libcanberra-gtk* libgtk-3-dev libgstreamer1.0-dev gstreamer1.0-gtk3 libgstreamer-plugins-base1.0-dev gstreamer1.0-gl libxvidcore-dev libx264-dev python-dev python-numpy python3-pip python3-dev python3-numpy libtbb2 libtbb-dev libdc1394-22-dev libv4l-dev v4l-utils libopenblas-dev libatlas-base-dev libblas-dev liblapack-dev gfortran libhdf5-dev libprotobuf-dev libgoogle-glog-dev libgflags-dev protobuf-compiler && \
+echo "installing build tools"
+	apt-get update
+	apt-get install -y pip &&
+	apt-get install -y wget systemd unzip python3 curl make g++ build-essential libvlc-dev vlc libx11-dev ffmpeg build-essential cmake git unzip pkg-config gfortran libjpeg-dev libtiff-dev libpng-dev libavcodec-dev libavformat-dev libswscale-dev libgtk2.0-dev libcanberra-gtk* libgtk-3-dev libgstreamer1.0-dev gstreamer1.0-gtk3 libgstreamer-plugins-base1.0-dev gstreamer1.0-gl libxvidcore-dev libx264-dev python-dev python3-pip python3-dev python3-numpy libtbb2 libtbb-dev libdc1394-22-dev libv4l-dev v4l-utils libopenblas-dev libatlas-base-dev libblas-dev liblapack-dev gfortran libhdf5-dev libprotobuf-dev libgoogle-glog-dev libgflags-dev protobuf-compiler
+	python3 -m pip install numpy
 
-echo "Instal OpenCV"
+echo "Install OpenCV"
   cd ~
   wget -O opencv.zip https://github.com/opencv/opencv/archive/4.5.4.zip
   wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/4.5.4.zip
@@ -41,7 +43,7 @@ echo "Instal OpenCV"
   -D INSTALL_PYTHON_EXAMPLES=OFF \
   -D BUILD_opencv_python3=TRUE \
   -D OPENCV_GENERATE_PKGCONFIG=ON \
-  -D BUILD_EXAMPLES=OFF ..
+  -D BUILD_EXAMPLES=OFF .. 
   make -j4
   make install
   ldconfig
@@ -49,15 +51,15 @@ echo "Instal OpenCV"
   echo "Congratulations!"
   echo "You've successfully installed OpenCV"
 
-echo "Instal AgentDVR"
+echo "Install AgentDVR"
   cd ~
   mkdir AgentDVR
   cd AgentDVR
-	curl --show-error --location "https://ispyfiles.azureedge.net/downloads/Agent_ARM32_3_7_6_0.zip" -o "AgentDVR.zip" && \
-	unzip AgentDVR.zip && \
+	curl --show-error --location "https://ispyfiles.azureedge.net/downloads/Agent_ARM32_3_7_6_0.zip" -o "AgentDVR.zip"
+	unzip AgentDVR.zip
 	rm AgentDVR.zip
 
-echo "Instal Dotnet"
+echo "Install Dotnet"
   ABSOLUTE_PATH=~
   cd ~
 	echo -n "Install dotnet 3.1.300 for Agent"
@@ -71,7 +73,7 @@ echo "Set AgentDVR as service"
   chmod 644 ./AgentDVR.service
   chown root -R $ABSOLUTE_PATH/AgentDVR
   cp AgentDVR.service /etc/systemd/system/AgentDVR.service
-  systemctl daemon-reload
-  systemctl enable AgentDVR.service
+#  systemctl daemon-reload
+#  systemctl enable AgentDVR.service
 
 exit
