a3de364bafa3cabad8fb5f2382973c5aa1630d17
Jan Kuthan <kdosiodjinud@gmail.com>
2022-07-14 22:22:35 +0200

    Fix amd64 version

--- homeassistant-agent-dvr/install.sh
+++ homeassistant-agent-dvr/install.sh
@@ -54,7 +54,8 @@ echo "Install AgentDVR"
   cd ~
   mkdir AgentDVR
   cd AgentDVR
-	curl --show-error --location "https://ispyfiles.azureedge.net/downloads/Agent_ARM32_3_7_6_0.zip" -o "AgentDVR.zip"
+#	curl --show-error --location "https://ispyfiles.azureedge.net/downloads/Agent_ARM32_3_7_6_0.zip" -o "AgentDVR.zip"
+	curl --show-error --location "https://ispyfiles.azureedge.net/downloads/Agent_Linux64_4_0_9_0.zip" -o "AgentDVR.zip"
 	unzip AgentDVR.zip
 	rm AgentDVR.zip
 
