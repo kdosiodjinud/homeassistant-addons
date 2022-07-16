eb7c5f4993c43750a40e32bee39b6472e6c55d85
Jan Kuthan <kdosiodjinud@gmail.com>
2022-07-16 18:15:03 +0200

    Fix

--- homeassistant-agent-dvr/links.sh
+++ homeassistant-agent-dvr/links.sh
@@ -4,22 +4,22 @@ echo "Link data to persist in hassio"
   cd /home/agentdvr/AgentDVR
 
   mkdir -p /data/XML
-  mv XML /data/XMLTmp
+  mv XML -n /data/XMLTmp
   rm -rf /home/agentdvr/AgentDVR/XML
   ln -s /data/XML/
-  mv XMLTmp ./XML
+  mv -n XMLTmp ./XML
 
   mkdir -p /data/Commands
-  mv Commands /data/CommandsTmp
+  mv -n Commands /data/CommandsTmp
   rm -rf /home/agentdvr/AgentDVR/Commands
   ln -s /data/Commands/
-  mv CommandsTmp ./Commands
+  mv -n CommandsTmp ./Commands
 
   mkdir -p /data/Media
-  mv Media MediaTmp
+  mv -n Media MediaTmp
   rm -rf /home/agentdvr/AgentDVR/Media
   ln -s /data/Media/
-  mv MediaTmp ./Media
+  mv -n MediaTmp ./Media
 
   chmod -R 777 /home/agentdvr
   chmod -R 777 /data/XML
