8c97525e8533f01405491f26a4608171b39b3b6e
Jan Kuthan <kdosiodjinud@gmail.com>
2022-07-19 18:30:46 +0200

    Fix

--- homeassistant-agent-dvr/run.sh
+++ homeassistant-agent-dvr/run.sh
@@ -8,13 +8,26 @@ sleep 10
 echo "Link data to persist in hassio"
   cd /home/agentdvr/AgentDVR
 
-
   if [ ! -d "/data/Media" ]
   then
     cp -R /home/agentdvr/AgentDVR/Media /data/
   fi
   mv /home/agentdvr/AgentDVR/Media /home/agentdvr/AgentDVR/Media-original
   cd /home/agentdvr/AgentDVR && ln -s /data/Media/
+
+  if [ ! -d "/data/XML" ]
+  then
+    cp -R /home/agentdvr/AgentDVR/XML /data/
+  fi
+  mv /home/agentdvr/AgentDVR/XML /home/agentdvr/AgentDVR/XML-original
+  cd /home/agentdvr/AgentDVR && ln -s /data/XML/
+
+  if [ ! -d "/data/Commands" ]
+  then
+    cp -R /home/agentdvr/AgentDVR/Commands /data/
+  fi
+  mv /home/agentdvr/AgentDVR/Commands /home/agentdvr/AgentDVR/Commands-original
+  cd /home/agentdvr/AgentDVR && ln -s /data/Commands/
 
   chmod -R 777 /data/
   chown -R agentdvr:agentdvr /data/
