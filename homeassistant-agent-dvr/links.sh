806f551f68452dc9bf1f6327ae31eb6a7f413d48
Jan Kuthan <kdosiodjinud@gmail.com>
2022-07-19 17:38:09 +0200

    Fix

--- homeassistant-agent-dvr/links.sh
+++ homeassistant-agent-dvr/links.sh
@@ -4,19 +4,51 @@ su agentdvr
 echo "Create configs"
 /home/agentdvr/AgentDVR/.dotnet/dotnet /home/agentdvr/AgentDVR/Agent.dll & sleep 10 && kill "$!"
 
+sleep 10
+
 echo "Link data to persist in hassio"
   cd /home/agentdvr/AgentDVR
 
-  mkdir -p /data/XML
-  rm -rf XML
-  ln -s /data/XML/
+  if [ ! -d "/data/XML" ]
+  then
+    mkdir -p /data/XML
+    mv XML /data/
+    ln -s /data/XML/
 
-  mkdir -p /data/Commands
-  rm -rf Commands
-  ln -s /data/Commands/
+    chmod -R 777 /data/XML
+    chown -R agentdvr:agentdvr /data/XML
+  else
+    rm -rf XML
+    ln -s /data/XML/
+  fi
 
-  mkdir -p /data/Media
-  rm -rf Media
-  ln -s /data/Media/
+  if [ ! -d "/data/Commands" ]
+  then
+    mkdir -p /data/Commands
+    mv Commands /data/
+    ln -s /data/Commands/
+
+    chmod -R 777 /data/Commands
+    chown -R agentdvr:agentdvr /data/Commands
+  else
+    rm -rf Commands
+    ln -s /data/Commands/
+  fi
+
+  if [ ! -d "/data/Media" ]
+  then
+    mkdir -p /data/Media
+    mv Media /data/
+    ln -s /data/Media/
+
+    chmod -R 777 /data/Media
+    chown -R agentdvr:agentdvr /data/Media
+  else
+    rm -rf Media
+    ln -s /data/Media/
+  fi
+
+  chmod -R 777 /home/agentdvr
+  chown -R agentdvr:agentdvr /home/agentdvr
 
   exit 0;
