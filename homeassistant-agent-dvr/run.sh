7c6431e186d88c331812934f8db1f6a231186fa4
Jan Kuthan <kdosiodjinud@gmail.com>
2022-07-19 17:46:25 +0200

    Fix

--- homeassistant-agent-dvr/run.sh
+++ homeassistant-agent-dvr/run.sh
@@ -1,12 +1,47 @@
 #!/bin/bash
 
 su agentdvr
+
+echo "Create configs"
+/home/agentdvr/AgentDVR/.dotnet/dotnet /home/agentdvr/AgentDVR/Agent.dll & sleep 10 && kill "$!"
+
+sleep 10
+
+echo "Link data to persist in hassio"
+  cd /home/agentdvr/AgentDVR
+
+  if [ ! -d "/data/XML" ]
+  then
+    mkdir -p /data/XML
+    mv XML /data/
+  else
+    rm -rf XML
+  fi
+  ln -s /data/XML/
+
+  if [ ! -d "/data/Commands" ]
+  then
+    mkdir -p /data/Commands
+    mv Commands /data/
+  else
+    rm -rf Commands
+  fi
+  ln -s /data/Commands/
+
+  if [ ! -d "/data/Media" ]
+  then
+    mkdir -p /data/Media
+    mv Media /data/
+  else
+    rm -rf Media
+  fi
+  ln -s /data/Media/
+
 echo "Run AgentDVR with persisted configuration"
 /home/agentdvr/AgentDVR/.dotnet/dotnet /home/agentdvr/AgentDVR/Agent.dll
 
 for (( ; ; ))
 do
-   echo "Container with Agent DVR!"
-  whoami
+   echo "Agent DVR failed!"
    sleep 60
 done
