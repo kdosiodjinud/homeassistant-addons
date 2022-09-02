6550d6e3df0cf9d6497c58f2a9fde23eef895aab
Jan Kuthan <kdosiodjinud@gmail.com>
2022-09-02 20:21:27 +0200

    Upgrade AgentDVR

--- homeassistant-agent-dvr-prepare-server/install.sh
+++ homeassistant-agent-dvr-prepare-server/install.sh
@@ -6,21 +6,14 @@ echo "installing build tools"
 
 apt-get update && apt-get install --no-install-recommends -y unzip python3 curl make g++ build-essential libvlc-dev vlc libx11-dev libtbb-dev libc6-dev gss-ntlmssp libusb-1.0-0-dev apt-transport-https libatlas-base-dev alsa-utils libxext-dev ffmpeg
 
-FILE=$ABSOLUTE_PATH/start_agent.sh
-if [ ! -f $FILE ]
-then
-	echo "downloading start script"
-	curl --show-error --location "https://raw.githubusercontent.com/ispysoftware/agent-install-scripts/main/start_agent.sh" -o "start_agent.sh"
-	chmod a+x ./start_agent.sh
-fi
-
 mkdir AgentDVR
 cd AgentDVR
 
 FILE=$ABSOLUTE_PATH/AgentDVR/Agent.dll
 
-echo "finding installer for amd64"
-AGENTURL=$(curl -s --fail "https://www.ispyconnect.com/api/Agent/DownloadLocation2?productID=24&is64=true&platform=Linux" | tr -d '"')
+echo "finding installer"
+AGENTURL=$(curl -s --fail "https://www.ispyconnect.com/api/Agent/DownloadLocation4?platform=Linux64&fromVersion=0" | tr -d '"')
+
 echo "Downloading $AGENTURL"
 curl --show-error --location "$AGENTURL" -o "AgentDVR.zip"
 unzip AgentDVR.zip
